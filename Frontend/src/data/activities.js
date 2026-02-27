c9fa7215c79ebf99fc77814272ffa9b70562fd7c
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-02-26 23:22:12 -0500

    Update and rename .gitkeep to activities.js

---
+++ Frontend/src/data/activities.js
@@ -0,0 +1,17 @@
+
+export const activities = [
+  {
+    id: 1,
+    titulo: "Estudiar React",
+    categoria: "Programación Web",
+    fecha: "2026-02-27",
+    estado: "Pendiente",
+  },
+  {
+    id: 2,
+    titulo: "Hacer ejercicio",
+    categoria: "Personal",
+    fecha: "2026-02-27",
+    estado: "Completada",
+  },
+];
