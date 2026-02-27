1ee1f362bea902b1528b31f6a65d197f2c68c311
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-02-27 12:54:14 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hoy from "./pages/Hoy";
-import CrearActividad from "./pages/CrearActividad";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Hoy />} />
-        <Route path="/crear" element={<CrearActividad />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Hoy />;
 }
 
 export default App;
