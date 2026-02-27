f4556b4a8abd17c70896efc7cacb16adc2b2276a
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-02-26 23:23:39 -0500

    Update CrearActividad.jsx

--- Frontend/src/pages/CrearActividad.jsx
+++ Frontend/src/pages/CrearActividad.jsx
@@ -1,8 +1,21 @@
-export default function CrearActividad() {
+import { useNavigate } from "react-router-dom";
+import { addActivity } from "../services/activitiesService";
+import ActivityForm from "../components/ActivityForm";
+
+function CrearActividad() {
+  const navigate = useNavigate();
+
+  const handleCreate = (data) => {
+    addActivity(data);
+    navigate("/");
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Crear Actividad</h1>
-      <p>Aquí irá el formulario para crear actividades.</p>
+      <ActivityForm onSubmit={handleCreate} />
     </div>
   );
 }
+
+export default CrearActividad;
