790a7e662fd7ea7cea6ac6a49f381884a5629d0b
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-02-27 12:58:42 -0500

    Update ActivityCard.jsx

--- Frontend/src/components/ActivityCard.jsx
+++ Frontend/src/components/ActivityCard.jsx
@@ -1,27 +1,23 @@
-import "./ActivityCard.css";
+import "../styles/App.css";
 
 export default function ActivityCard({ activity, onToggle }) {
   return (
     <div className={`card ${activity.estado === "Completada" ? "done" : ""}`}>
-      <div className="card-header">
-        <h3>{activity.titulo}</h3>
-      </div>
+      <h3>{activity.titulo}</h3>
 
-      <div className="card-body">
-        <p className="materia">{activity.materia}</p>
-        <p className="fecha">{activity.fecha}</p>
+      <p className="materia">{activity.materia}</p>
+      <p className="fecha">{activity.fecha}</p>
 
-        <span
-          className={`badge ${
-            activity.estado === "Completada"
-              ? "badge-success"
-              : "badge-pending"
-          }`}
-          onClick={onToggle}
-        >
-          {activity.estado}
-        </span>
-      </div>
+      <span
+        className={`badge ${
+          activity.estado === "Completada"
+            ? "badge-success"
+            : "badge-pending"
+        }`}
+        onClick={onToggle}
+      >
+        {activity.estado}
+      </span>
     </div>
   );
 }
