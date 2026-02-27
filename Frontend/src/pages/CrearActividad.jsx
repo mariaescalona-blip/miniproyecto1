4a800d003be98dd517840754229b8a3d663d5c44
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-02-26 22:16:21 -0500

    crear pagina CrearActividad

---
+++ Frontend/src/pages/CrearActividad.jsx
@@ -0,0 +1,8 @@
+export default function CrearActividad() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Crear Actividad</h1>
+      <p>Aquí irá el formulario para crear actividades.</p>
+    </div>
+  );
+}
