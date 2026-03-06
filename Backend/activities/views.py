a8effa1d98a754fe40db5edaa640176f813a0def
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:01:21 -0500

    Update views.py

--- Backend/activities/views.py
+++ Backend/activities/views.py
@@ -1,7 +1,30 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Activity
 from .serializers import ActivitySerializer
 
+
 class ActivityViewSet(viewsets.ModelViewSet):
-    queryset = Activity.objects.all()
+    queryset = Activity.objects.all().order_by('-creado')
     serializer_class = ActivitySerializer
+
+
+@api_view(['GET'])
+def progreso(request):
+
+    total = Activity.objects.count()
+    completadas = Activity.objects.filter(
+        estado='completada'
+    ).count()
+
+    porcentaje = 0
+
+    if total > 0:
+        porcentaje = (completadas / total) * 100
+
+    return Response({
+        "total": total,
+        "completadas": completadas,
+        "porcentaje": porcentaje
+    })
