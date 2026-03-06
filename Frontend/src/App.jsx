901b9f6a7b5fb3c50716f9cbe9541f25031d0e50
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-05 22:17:13 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import Hoy from './pages/Hoy.jsx'
-import CrearActividad from './pages/CrearActividad.jsx'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Hoy from "./pages/Hoy.jsx";
+import CrearActividad from "./pages/CrearActividad.jsx";
+
+function DetalleActividad() {
+  return <h2>Detalle de actividad</h2>;
+}
+
+function Progreso() {
+  return <h2>Vista de progreso</h2>;
+}
 
 export default function App() {
   return (
-    <div className="app-root">
-      <header className="app-header">
-        <Link to="/" className="brand">TaskFlow</Link>
-        <nav>
-          <Link to="/">Hoy</Link> | <Link to="/crear">Crear actividad</Link>
-        </nav>
-      </header>
-
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Hoy />} />
-          <Route path="/crear" element={<CrearActividad />} />
-        </Routes>
-      </main>
+    <div>
+      <Routes>
+        <Route path="/" element={<Hoy />} />
+        <Route path="/crear" element={<CrearActividad />} />
+        <Route path="/actividad/:id" element={<DetalleActividad />} />
+        <Route path="/progreso" element={<Progreso />} />
+      </Routes>
     </div>
-  )
+  );
 }
