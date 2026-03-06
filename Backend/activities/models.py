683098bdc63b926d8b4d673c43e62d8fc5d0de13
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:08:39 -0500

    Update models.py

--- Backend/activities/models.py
+++ Backend/activities/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Activity(models.Model):
 
     titulo = models.CharField(max_length=200)
@@ -16,6 +17,22 @@ class Activity(models.Model):
     )
 
     creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+
+class SubTask(models.Model):
+
+    actividad = models.ForeignKey(
+        Activity,
+        related_name="subtasks",
+        on_delete=models.CASCADE
+    )
+
+    titulo = models.CharField(max_length=200)
+
+    completada = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo
