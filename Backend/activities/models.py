103a9e9c4b0b1c32b90a08196816a2be9effa73f
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:42:36 -0500

    Update models.py

--- Backend/activities/models.py
+++ Backend/activities/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Activity(models.Model):
 
     titulo = models.CharField(max_length=200)
+
     descripcion = models.TextField(blank=True)
+
     fecha = models.DateField()
 
     estado = models.CharField(
