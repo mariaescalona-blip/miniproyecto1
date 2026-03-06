70795f902da59052d94f080fe0484c6073c5dfff
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 12:07:43 -0500

    Update ActivityForm.jsx

--- Frontend/src/components/ActivityForm.jsx
+++ Frontend/src/components/ActivityForm.jsx
@@ -1,62 +1,53 @@
 import { useState } from "react"
 import { createActivity } from "../services/activitiesService"
 
-function ActivityForm({ onCreated }) {
+function ActivityForm() {
 
   const [titulo, setTitulo] = useState("")
   const [descripcion, setDescripcion] = useState("")
   const [fecha, setFecha] = useState("")
 
   const handleSubmit = async (e) => {
+
     e.preventDefault()
 
-    const nuevaActividad = {
+    await createActivity({
       titulo,
       descripcion,
       fecha
-    }
+    })
 
-    try {
+    alert("Actividad creada")
 
-      const activity = await createActivity(nuevaActividad)
-
-      if (onCreated) {
-        onCreated(activity)
-      }
-
-      setTitulo("")
-      setDescripcion("")
-      setFecha("")
-
-    } catch (error) {
-      console.error(error)
-    }
+    setTitulo("")
+    setDescripcion("")
+    setFecha("")
   }
 
   return (
     <form onSubmit={handleSubmit}>
 
       <input
-        placeholder="Titulo"
+        type="text"
+        placeholder="Título"
         value={titulo}
-        onChange={(e)=>setTitulo(e.target.value)}
+        onChange={(e) => setTitulo(e.target.value)}
       />
 
       <input
-        placeholder="Descripcion"
+        type="text"
+        placeholder="Descripción"
         value={descripcion}
-        onChange={(e)=>setDescripcion(e.target.value)}
+        onChange={(e) => setDescripcion(e.target.value)}
       />
 
       <input
         type="date"
         value={fecha}
-        onChange={(e)=>setFecha(e.target.value)}
+        onChange={(e) => setFecha(e.target.value)}
       />
 
-      <button type="submit">
-        Crear actividad
-      </button>
+      <button type="submit">Crear actividad</button>
 
     </form>
   )
