6e76d01baf4010afbbf1d701981085ec2aba7e0d
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 00:36:26 -0500

    Create views.py

---
+++ Backend/activities/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from .models import Activity
+from .serializers import ActivitySerializer
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+
+    queryset = Activity.objects.all()
+
+    serializer_class = ActivitySerializer
