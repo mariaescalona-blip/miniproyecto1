7d610f0a4f2da5819217ba260ae0f66662b39481
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:09:08 -0500

    Update views.py

--- Backend/activities/views.py
+++ Backend/activities/views.py
@@ -1,19 +1,28 @@
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Activity
-from .serializers import ActivitySerializer
+
+from .models import Activity, SubTask
+from .serializers import ActivitySerializer, SubTaskSerializer
 
 
 class ActivityViewSet(viewsets.ModelViewSet):
+
     queryset = Activity.objects.all().order_by('-creado')
     serializer_class = ActivitySerializer
+
+
+class SubTaskViewSet(viewsets.ModelViewSet):
+
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskSerializer
 
 
 @api_view(['GET'])
 def progreso(request):
 
     total = Activity.objects.count()
+
     completadas = Activity.objects.filter(
         estado='completada'
     ).count()
