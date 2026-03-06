cebbc78efbeabab36a46d4b6861f8f781a3883a5
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 12:06:37 -0500

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,41 +1,10 @@
-/*
-Archivo principal que inicia la aplicación React.
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./styles/global.css"
 
-Este archivo:
-
-1. Importa React
-2. Conecta React con el HTML
-3. Activa el sistema de rutas
-4. Renderiza la aplicación
-*/
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App.jsx";
-import "./styles/global.css";
-
-/*
-createRoot busca el div con id="root"
-dentro del archivo index.html
-*/
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-  /*
-  StrictMode ayuda a detectar errores
-  durante el desarrollo
-  */
   <React.StrictMode>
-
-    {/* BrowserRouter habilita la navegación entre rutas */}
-    <BrowserRouter>
-
-      {/* Componente principal */}
-      <App />
-
-    </BrowserRouter>
-
+    <App />
   </React.StrictMode>
-
-);
+)
