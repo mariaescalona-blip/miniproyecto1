1c4cd114d64c7e67ef3493ff7d6942b4255a2de3
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-05 22:27:57 -0500

    Update activities.js

--- Frontend/src/data/activities.js
+++ Frontend/src/data/activities.js
@@ -1,17 +1,26 @@
+/*
+Datos iniciales del sistema.
+
+Se utilizan cuando el usuario
+abre la aplicación por primera vez.
+*/
 
 export const seedActivities = [
+
   {
-    id: '1',
-    title: 'Estudiar React - Programación Web',
-    subject: 'Programación Web',
-    date: '2026-02-27',
-    status: 'Pendiente'
+    id: 1,
+    title: "Estudiar React",
+    subject: "Programación Web",
+    date: "2026-02-27",
+    status: "Pendiente"
   },
+
   {
-    id: '2',
-    title: 'Hacer ejercicio',
-    subject: 'Personal',
-    date: '2026-02-27',
-    status: 'Completada'
+    id: 2,
+    title: "Hacer ejercicio",
+    subject: "Personal",
+    date: "2026-02-27",
+    status: "Completada"
   }
-]
+
+];
