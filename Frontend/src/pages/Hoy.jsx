5b090436b8ef5a51984235a2f7987e73411c17b3
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:25:32 -0500

    Update Hoy.jsx

--- Frontend/src/pages/Hoy.jsx
+++ Frontend/src/pages/Hoy.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react"
 import { getActivities } from "../services/activitiesService"
 import ActivityList from "../components/ActivityList"
+import ActivityForm from "../components/ActivityForm"
 
 function Hoy() {
 
   const [activities, setActivities] = useState([])
-
-  useEffect(() => {
-    loadActivities()
-  }, [])
 
   const loadActivities = async () => {
     const data = await getActivities()
     setActivities(data)
   }
 
+  useEffect(() => {
+    loadActivities()
+  }, [])
+
   return (
 
     <div>
 
-      <h1>Actividades de hoy</h1>
+      <h1>Gestor de Actividades</h1>
+
+      <ActivityForm reload={loadActivities} />
 
       <ActivityList
         activities={activities}
