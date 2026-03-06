79601b490bdea676f38c3d24af939a1197ad94a3
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 12:06:52 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,43 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
 import Hoy from "./pages/Hoy"
+import CrearActividad from "./pages/CrearActividad"
+import DetalleActividad from "./pages/DetalleActividad"
 import Progreso from "./pages/Progreso"
 
+import "./styles/App.css"
+
 function App() {
-
   return (
+    <Router>
 
-    <div>
+      <nav className="navbar">
+        <h2>Gestor de Actividades</h2>
 
-      <Hoy />
+        <div className="menu">
+          <Link to="/">Hoy</Link>
+          <Link to="/crear">Crear</Link>
+          <Link to="/progreso">Progreso</Link>
+        </div>
+      </nav>
 
-      <Progreso />
+      <div className="container">
 
-    </div>
+        <Routes>
 
+          <Route path="/" element={<Hoy />} />
+
+          <Route path="/crear" element={<CrearActividad />} />
+
+          <Route path="/actividad/:id" element={<DetalleActividad />} />
+
+          <Route path="/progreso" element={<Progreso />} />
+
+        </Routes>
+
+      </div>
+
+    </Router>
   )
 }
 
