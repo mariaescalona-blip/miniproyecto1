9d4509ef4ffb84f8d0f2ffcbd3b3c985f7f7b7e7
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:23:11 -0500

    Update activitiesService.js

--- Frontend/src/services/activitiesService.js
+++ Frontend/src/services/activitiesService.js
@@ -8,22 +8,34 @@ export const getActivities = async () => {
 export const createActivity = async (activity) => {
   const res = await fetch(`${API_URL}/activities/`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(activity)
   })
 
   return res.json()
 }
 
+export const deleteActivity = async (id) => {
+  await fetch(`${API_URL}/activities/${id}/`, {
+    method: "DELETE"
+  })
+}
+
 export const createSubTask = async (subtask) => {
   const res = await fetch(`${API_URL}/subtasks/`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(subtask)
+  })
+
+  return res.json()
+}
+
+export const toggleSubTask = async (id, completada) => {
+  const res = await fetch(`${API_URL}/subtasks/${id}/`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ completada })
   })
 
   return res.json()
