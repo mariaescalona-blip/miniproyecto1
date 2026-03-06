f6e874067cf8e3a762d7f7e2ff74513c90b5ec92
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:01:06 -0500

    Update serializers.py

--- Backend/activities/serializers.py
+++ Backend/activities/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Activity
 
 class ActivitySerializer(serializers.ModelSerializer):
+
+    def validate_titulo(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "El título debe tener al menos 3 caracteres"
+            )
+        return value
+
     class Meta:
         model = Activity
         fields = '__all__'
