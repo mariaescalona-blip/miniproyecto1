f25c94825239341249a9cd506d77af66f8f21879
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 11:45:09 -0500

    Update views.py

--- Backend/activities/views.py
+++ Backend/activities/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Activity, SubTask
 from .serializers import ActivitySerializer, SubTaskSerializer
@@ -8,32 +8,29 @@ from .serializers import ActivitySerializer, SubTaskSerializer
 
 class ActivityViewSet(viewsets.ModelViewSet):
 
-    queryset = Activity.objects.all()
-
+    queryset = Activity.objects.all().order_by("-fecha")
     serializer_class = ActivitySerializer
 
 
 class SubTaskViewSet(viewsets.ModelViewSet):
 
     queryset = SubTask.objects.all()
-
     serializer_class = SubTaskSerializer
 
 
 @api_view(["GET"])
 def progreso(request):
 
-    total = SubTask.objects.count()
-
-    completadas = SubTask.objects.filter(completada=True).count()
+    total = Activity.objects.count()
+    completadas = Activity.objects.filter(estado="completada").count()
 
     porcentaje = 0
 
     if total > 0:
-        porcentaje = (completadas / total) * 100
+        porcentaje = int((completadas / total) * 100)
 
     return Response({
         "total": total,
         "completadas": completadas,
-        "porcentaje": porcentaje
+        "progreso": porcentaje
     })
