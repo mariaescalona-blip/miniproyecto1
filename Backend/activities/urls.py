88cfb0afbdfd400267adc1e405832edc06e4f874
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:59:47 -0500

    Update urls.py

--- Backend/activities/urls.py
+++ Backend/activities/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from .views import ActivityViewSet, SubTaskViewSet, progreso
-
 
 router = DefaultRouter()
 
 router.register(r'activities', ActivityViewSet)
-
 router.register(r'subtasks', SubTaskViewSet)
-
 
 urlpatterns = [
 
