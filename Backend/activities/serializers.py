01c1826501dab7c4ef91ea4aa6f3671437b9f07c
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 10:42:50 -0500

    Update serializers.py

--- Backend/activities/serializers.py
+++ Backend/activities/serializers.py
@@ -6,20 +6,13 @@ class SubTaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubTask
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ActivitySerializer(serializers.ModelSerializer):
 
     subtasks = SubTaskSerializer(many=True, read_only=True)
 
-    def validate_titulo(self, value):
-        if len(value) < 3:
-            raise serializers.ValidationError(
-                "El título debe tener al menos 3 caracteres"
-            )
-        return value
-
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = "__all__"
