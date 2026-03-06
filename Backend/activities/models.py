bfecbbbf1e7690f9f86da82a34f90f2afa5f0544
ANDRES FELIPE CHICAIZA ORTIZ <andres.chicaiza@correounivalle.edu.co>
2026-03-06 11:44:42 -0500

    Update models.py

--- Backend/activities/models.py
+++ Backend/activities/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
-
 class Activity(models.Model):
 
     titulo = models.CharField(max_length=200)
-
     descripcion = models.TextField(blank=True)
-
     fecha = models.DateField()
 
     estado = models.CharField(
         max_length=20,
         choices=[
-            ('pendiente', 'Pendiente'),
-            ('completada', 'Completada')
+            ("pendiente", "Pendiente"),
+            ("completada", "Completada")
         ],
-        default='pendiente'
+        default="pendiente"
     )
 
     creado = models.DateTimeField(auto_now_add=True)
@@ -26,14 +23,13 @@ class Activity(models.Model):
 
 class SubTask(models.Model):
 
-    actividad = models.ForeignKey(
+    activity = models.ForeignKey(
         Activity,
         related_name="subtasks",
         on_delete=models.CASCADE
     )
 
     titulo = models.CharField(max_length=200)
-
     completada = models.BooleanField(default=False)
 
     def __str__(self):
