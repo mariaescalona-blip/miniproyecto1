d3233635d6be2c874838ab9757b3638718b9550e
mariaescalona-blip <maria.escalona@correounivalle.edu.co>
2026-04-10 12:31:35 -0500

    inclusion de un nuevo endpoint

--- Frontend/src/services/activitiesService.js
+++ Frontend/src/services/activitiesService.js
@@ -1,12 +1,17 @@
 const API_URL = "https://miniproyecto1-6.onrender.com/api"
 
 export const getActivities = async () => {
-  const res = await fetch(`${API_URL}/activities/`)
+  const res = await fetch(${API_URL}/activities/)
+  return res.json()
+}
+
+export const getToday = async () => {
+  const res = await fetch(${API_URL}/today/)
   return res.json()
 }
 
 export const createActivity = async (activity) => {
-  const res = await fetch(`${API_URL}/activities/`, {
+  const res = await fetch(${API_URL}/activities/, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
